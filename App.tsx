f198d09757ef0147a79903128ae62a31d022e994
Paulo-Gouvea <paulogouvea1994@gmail.com>
2022-01-17 10:42:31 -0300

    chore: added global theme for the application

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import { ThemeProvider } from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+import {
+  Poppins_400Regular,
+  Poppins_700Bold,
+  useFonts,
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View>
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold
+  });
 
-    </View>
-  );
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme} >
+      <Home />
+    </ThemeProvider>
+  );  
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+    Container,
+    Teste
+} from './styles';
+
+export function Home(){
+    return (
+        <Container>
+            <Teste>Olár</Teste>
+        </Container>
+    );
+}
